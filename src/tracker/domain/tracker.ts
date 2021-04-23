24bad60163f0fbfa8e52148103ca36c66b2c1910
felipelealdefaria <felipelealdefaria@gmail.com>
2021-04-23 15:31:21 -0300

    feat: create response types and new init options attrs

--- src/tracker/domain/tracker.ts
+++ src/tracker/domain/tracker.ts
@@ -1,41 +1,93 @@
 export interface Tracker {
-  init (params: InitParams): Promise<void>
-  log (params: LogParams): Promise<void>
-  logGroup (params: LogGroupParams): Promise<void>
+  setUserId (userId: string): StatusResponse
+  setUserProperties (userProperties: unknown): StatusResponse
+  init ({ apiKey, userId, options, callback }: InitParams): StatusResponse
+  log ({ eventName, eventProperties, timestamp, callback }: LogParams): StatusResponse
 
-  getSessionId (): number
-  isNewSession (): boolean
-  clearUserProperties (): Promise<void>
-  setUserId (userId: string): Promise<void>
-  setDomain (domain: string): Promise<void>
-  setTrackOff (enable: boolean): Promise<void>
-  setGroup (params: GroupParams): Promise<void>
-  setDeviceId (deviceId: string): Promise<void>
-  setUserProperties (userProperties: {}): Promise<void>
+  getSessionId (): SessionIdResponse
+  isNewSession (): NewSessionResponse
+  clearUserProperties (): StatusResponse
+  setDomain (domain: string): StatusResponse
+  setTrackOff (enable: boolean): StatusResponse
+  setDeviceId (deviceId: string): StatusResponse
+  setGroup ({ groupType, groupName }: GroupParams): StatusResponse
+  logGroup ({ eventName, eventProperties, groups, callback }: LogGroupParams): StatusResponse
 }
 
-type InitParams = {
+export type StatusResponse = {
+  error?: boolean
+  success?: boolean
+}
+
+export type SessionIdResponse = {
+  sessionId: number | null
+} & StatusResponse
+
+export type NewSessionResponse = {
+  isNewSession: boolean | null
+} & StatusResponse
+
+export type InitParams = {
   apiKey: string
   userId?: string
-  options?: {}
-  callback?: Function
+  options?: InitOptions
+  callback?: () => unknown
 }
 
-type LogParams = {
-  eventType: string
-  eventProperties?: {}
+export type LogParams = {
+  eventName: string
+  eventProperties?: unknown
   timestamp?: number
-  callback?: Function
+  callback?: () => unknown
 }
 
-type LogGroupParams = {
-  eventType: string
-  eventProperties?: {}
-  groups?: {}
-  callback?: Function
+export type LogGroupParams = {
+  eventName: string
+  eventProperties?: unknown
+  groups?: unknown
+  callback?: () => unknown
 }
 
-type GroupParams = {
+export type GroupParams = {
   groupType: string
   groupName: string | string[]
+}
+
+export type InitOptions = {
+  saveEvents?: boolean
+  includeUtm?: boolean
+  includeGclid?: boolean
+  includeReferrer?: boolean
+  eventUploadPeriodMillis?: number
+  saveParamsReferrerOncePerSession?: boolean
+
+  // see in: https://amplitude.github.io/Amplitude-JavaScript/Options/
+  domain?: string
+  optOut?: string
+  onError?: string
+  storage?: string
+  language?: string
+  platform?: string
+  logLevel?: string
+  deviceId?: string
+  unsentKey?: string
+  forceHttps?: string
+  apiEndpoint?: string
+  batchEvents?: string
+  secureCookie?: string
+  includeFbclid?: string
+  savedMaxCount?: string
+  sessionTimeout?: string
+  sameSiteCookie?: string
+  disableCookies?: string
+  trackingOptions?: string
+  uploadBatchSize?: string
+  cookieExpiration?: string
+  unsentIdentifyKey?: string
+  cookieForceUpgrade?: string
+  deferInitialization?: string
+  deviceIdFromUrlParam?: string
+  eventUploadThreshold?: string
+  logAttributionCapturedEvent?: string
+  unsetParamsReferrerOnNewSession?: string
 }
