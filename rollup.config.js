d93d4d33255b05eb54a80f691a0753ad86779b21
felipelealdefaria <felipelealdefaria@gmail.com>
2021-04-27 15:53:49 -0300

    fix: rename input dist and src path

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/tracker/index.ts',
   output: [
     {
       file: pkg.main,
