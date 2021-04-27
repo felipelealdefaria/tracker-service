3ba293f44f2ae86fa67de5d507d3e726b45099a6
felipelealdefaria <felipelealdefaria@gmail.com>
2021-04-27 10:52:13 -0300

    feat: create mock data with fake values

--- src/tracker/__tests__/mock-data.ts
+++ src/tracker/__tests__/mock-data.ts
@@ -3,8 +3,16 @@ import faker from 'faker'
 export const mock = {
   options: {},
   eventName: 'cta_clicked',
+  domain: faker.random.word(),
   apiKey: faker.random.uuid(),
+  groups: faker.random.words(),
+  deviceId: faker.random.word(),
   userId: faker.internet.email(),
+  enable: faker.random.boolean(),
+  groupName: faker.random.word(),
+  groupType: faker.random.word(),
+  sessionId: faker.random.number(),
+  isNewSession: faker.random.boolean(),
   callback: jest.fn(() => alert('called event')),
 
   eventProperties: {
