bf3b01dd8aee3f53c1889ad050cddc0e267ba5fc
felipelealdefaria <felipelealdefaria@gmail.com>
2021-04-27 10:50:50 -0300

    refactor: type response in tracker service

--- src/tracker/data/tracker-service.ts
+++ src/tracker/data/tracker-service.ts
@@ -4,110 +4,74 @@ export class TrackerService implements Types.Tracker {
   constructor (private readonly tracker: TrackerParams) {}
 
   init ({ apiKey, userId, options, callback }: Types.InitParams): Types.StatusResponse {
-    try {
-      this.tracker.init({ apiKey, userId, options, callback })
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.init({ apiKey, userId, options, callback })
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 
   log ({ eventName, eventProperties, timestamp, callback }: Types.LogParams): Types.StatusResponse {
-    try {
-      this.tracker.logEvent({ eventName, eventProperties, timestamp, callback })
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.logEvent({ eventName, eventProperties, timestamp, callback })
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 
   setUserId (userId: string): Types.StatusResponse {
-    try {
-      this.tracker.setUserId(userId)
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.setUserId(userId)
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 
   setUserProperties (userProperties: {}): Types.StatusResponse {
-    try {
-      this.tracker.setUserProperties(userProperties)
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.setUserProperties(userProperties)
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 
   getSessionId (): Types.SessionIdResponse {
-    try {
-      const sessionId = this.tracker.getSessionId()
-      return { success: true, sessionId: sessionId }
-    } catch (err) {
-      return { error: true, sessionId: null }
-    }
+    const res: Types.SessionIdResponse = this.tracker.getSessionId()
+    if (res?.success) return { success: true, sessionId: res?.sessionId }
+    return { error: true, sessionId: null }
   }
 
   isNewSession (): Types.NewSessionResponse {
-    try {
-      const isNewSession = this.tracker.isNewSession()
-      return { success: true, isNewSession: isNewSession }
-    } catch (err) {
-      return { error: true, isNewSession: null }
-    }
+    const res: Types.NewSessionResponse = this.tracker.isNewSession()
+    if (res?.success) return { success: true, isNewSession: res?.isNewSession }
+    return { error: true, isNewSession: null }
   }
 
   clearUserProperties (): Types.StatusResponse {
-    try {
-      this.tracker.clearUserProperties()
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.clearUserProperties()
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 
   setDomain (domain: string): Types.StatusResponse {
-    try {
-      this.tracker.setDomain(domain)
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.setDomain(domain)
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 
   setTrackOff (enable: boolean): Types.StatusResponse {
-    try {
-      this.tracker.setTrackOff(enable)
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.setTrackOff(enable)
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 
   setGroup (params: Types.GroupParams): Types.StatusResponse {
-    try {
-      this.tracker.setGroup(params)
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.setGroup(params)
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 
   setDeviceId (deviceId: string): Types.StatusResponse {
-    try {
-      this.tracker.setDeviceId(deviceId)
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.setDeviceId(deviceId)
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 
   logGroup (params: Types.LogGroupParams): Types.StatusResponse {
-    try {
-      this.tracker.logGroup(params)
-      return { success: true }
-    } catch (err) {
-      return { error: true }
-    }
+    const res: Types.StatusResponse = this.tracker.logGroup(params)
+    if (res?.success) return { success: true }
+    return { error: true }
   }
 }
